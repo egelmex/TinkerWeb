032f54da797806c608a17e7d8a75a5753a0018e7
Simon Cooksey <sjc80@kent.ac.uk>
2013-09-15 21:35:21 +0100

    Removed the indentation from code to prevent the markdown processor from going crazy over the output

--- _plugins/youtube_tag.rb
+++ _plugins/youtube_tag.rb
@@ -8,10 +8,10 @@ module Jekyll
 
     def render(context)
       <<-eos
-      <div class="videoWrapper">
-           <!-- Copy & Pasted from YouTube -->
-           <iframe width="560" height="349" src="http://www.youtube.com/embed/#{@videoID}" frameborder="0" allowfullscreen></iframe>
-      </div>
+<div class="videoWrapper">
+<!-- Copy & Pasted from YouTube -->
+<iframe width="560" height="349" src="http://www.youtube.com/embed/#{@videoID}" frameborder="0" allowfullscreen></iframe>
+</div>
       eos
     end
   end
