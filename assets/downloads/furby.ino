225b1e6de2ff69ad0ae7a046bbeb599b258a904e
egelmex <ellism88@gmail.com>
2013-10-31 17:29:28 +0000

    Small fixes

--- assets/downloads/furby.ino
+++ assets/downloads/furby.ino
@@ -1,3 +1,16 @@
+// FurbyControl Simple Arduino sketch to control the Furby  Copyright (C) 2013 Daniel Knox
+
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 // Furby's pins are done at compiler time, so lets use a IO performance library
 #define DIGITALIO_NO_INTERRUPT_SAFETY
 #define DIGITALIO_NO_MIX_ANALOGWRITE
